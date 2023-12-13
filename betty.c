47d4dadaa04d84472ddd3f5d9cfe2a2832b49668
Chinwe-MC <chichuks02@gmail.com>
2023-12-13 10:23:36 -0800

    To pass betty style

---
+++ betty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Function name
+ *@void: Parameter name
+ *Return: void
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("i = %d", i)
+	}
+}
